b4f4adfee278cc83a8c1276ceec20d2e52fb9345
cajunkee <75953821+cajunkee@users.noreply.github.com>
2021-03-03 20:02:21 -0600

    changes questions

--- src/com/madlib/Main.java
+++ src/com/madlib/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner userInput = new Scanner(System.in);
-		System.out.println("Enter the name of a friend:");
+		System.out.println("Enter the name of your favorite celebrity:");
 		String name = userInput.nextLine();
 		System.out.println("Name an adverb:");
 		String adverb = userInput.nextLine();
@@ -16,16 +16,16 @@ public class Main {
 		String noun = userInput.nextLine();
 		System.out.println("Give me another noun:");
 		String nounTwo = userInput.nextLine();
-		System.out.println("What's your lucky number?");
+		System.out.println("How many siblings do you have?");
 		String number = userInput.nextLine();
-		System.out.println("What is YOUR name?");
+		System.out.println("What is your alias?");
 		String yourName = userInput.nextLine();
 		
 		System.out.printf("Here is your story: \n");
-		System.out.printf("\n Dear &s,", name);
-		System.out.printf("\n You are &s %s and want to be your %s!!", adverb, adjective, noun);
-		System.out.printf("\n I want to go to the %s with you in % days. Counting down!!", nounTwo , number);
-		System.out.printf("\n Sincerely and forever your homie, \n", yourName);
+		System.out.printf("\n Dear %s,", name);
+		System.out.printf("\n You are %s %s and I want to be your %s!!", adverb, adjective, noun);
+		System.out.printf("\n I want to go to the %s with you in %s days. Counting down!!", nounTwo , number);
+		System.out.printf("\n Sincerely and forever your homie, \n %s \n", yourName);
 		
 		
 		
